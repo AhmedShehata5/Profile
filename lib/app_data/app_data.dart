4f67a51c13a409139c35f409c5cd0b986374041e
Ahmed Shehata <65680614+AhmedShehata5@users.noreply.github.com>
2021-01-02 13:27:33 +0200

    profile

---
+++ lib/app_data/app_data.dart
@@ -0,0 +1,114 @@
+import 'package:user_profile_page_flutter/models/profile.dart';
+
+class AppData {
+  static List<Profile> profiles = [
+    Profile(
+      imageUrl: 'assets/1.jpeg',
+      title: 'iOS Developer',
+      subtitle:
+          'Book created for educational purposes and is not affiliated with iOS® Developer group(s), company(s) nor Stack Overflow. All trademarks belong to their respective company owners',
+      totalRates: '⭐⭐⭐⭐⭐',
+      totalFollowers: '12.4m',
+      totalDownloading: '13.2m',
+    ),
+    Profile(
+      imageUrl: 'assets/2.jpeg',
+      title: 'Objective-C',
+      subtitle:
+          'Book created for educational purposes and is not affiliated with Objective-C® group(s), company(s) nor Stack Overflow. All trademarks belong to their respective company owners',
+      totalRates: '️⭐⭐⭐⭐⭐',
+      totalFollowers: '15.4m',
+      totalDownloading: '1.2m',
+    ),
+    Profile(
+      imageUrl: 'assets/3.jpeg',
+      title: 'VBA',
+      subtitle:
+          'Book created for educational purposes and is not affiliated with VBA group(s), company(s) nor Stack Overflow. All trademarks belong to their respective company owners',
+      totalRates: '⭐⭐⭐',
+      totalFollowers: '1.4m',
+      totalDownloading: '1.2m',
+    ),
+    Profile(
+      imageUrl: 'assets/4.jpeg',
+      title: 'Haskell',
+      subtitle:
+          'Book created for educational purposes and is not affiliated with Haskell group(s), company(s) nor Stack Overflow. All trademarks belong to their respective company owners',
+      totalRates: '⭐⭐⭐⭐',
+      totalFollowers: '1.4m',
+      totalDownloading: '16.2m',
+    ),
+    Profile(
+      imageUrl: 'assets/5.jpeg',
+      title: 'MATLAB',
+      subtitle:
+          'Book created for educational purposes and is not affiliated with MATLAB® group(s), company(s) nor Stack Overflow. All trademarks belong to their respective company owners',
+      totalRates: '⭐⭐⭐⭐',
+      totalFollowers: '1.4m',
+      totalDownloading: '551.2m',
+    ),
+    Profile(
+      imageUrl: 'assets/6.jpeg',
+      title: 'PostgreSQL',
+      subtitle:
+          'Book created for educational purposes and is not affiliated with PostgreSQL® group(s), company(s) nor Stack Overflow. All trademarks belong to their respective company owners',
+      totalRates: '⭐⭐⭐⭐⭐',
+      totalFollowers: '1.4m',
+      totalDownloading: '5.2m',
+    ),
+    Profile(
+      imageUrl: 'assets/7.jpeg',
+      title: 'Visual Basic',
+      subtitle:
+          'Book created for educational purposes and is not affiliated with Visual Basic® .NET group(s), company(s) nor Stack Overflow. All trademarks belong to their respective company owners',
+      totalRates: '⭐⭐⭐',
+      totalFollowers: '19.4m',
+      totalDownloading: '81.2m',
+    ),
+    Profile(
+      imageUrl: 'assets/8.jpeg',
+      title: 'Ruby® on Rails',
+      subtitle:
+          'Book created for educational purposes and is not affiliated with Ruby® on Rails group(s), company(s) nor Stack Overflow. All trademarks belong to their respective company owners',
+      totalRates: '⭐⭐⭐⭐',
+      totalFollowers: '15.4m',
+      totalDownloading: '1.2m',
+    ),
+    Profile(
+      imageUrl: 'assets/9.jpeg',
+      title: 'AngularJS',
+      subtitle:
+          'Book created for educational purposes and is not affiliated with AngularJS group(s), company(s) nor Stack Overflow. All trademarks belong to their respective company owners',
+      totalRates: '⭐⭐⭐⭐',
+      totalFollowers: '14.4m',
+      totalDownloading: '1.2m',
+    ),
+    Profile(
+      imageUrl: 'assets/10.jpeg',
+      title: 'Excel® VBA',
+      subtitle:
+          'Book created for educational purposes and is not affiliated with Excel® VBA group(s), company(s) nor Stack Overflow. All trademarks belong to their respective company owners',
+      totalRates: '⭐⭐⭐⭐',
+      totalFollowers: '1.4m',
+      totalDownloading: '1.2m',
+    ),
+    Profile(
+      imageUrl: 'assets/11.jpeg',
+      title: 'Kotlin®',
+      subtitle:
+          'Book created for educational purposes and is not affiliated with Kotlin® group(s), company(s) nor Stack Overflow. All trademarks belong to their respective company owners',
+      totalRates: '⭐⭐⭐',
+      totalFollowers: '12.4m',
+      totalDownloading: '1.2m',
+    ),
+    Profile(
+      imageUrl: 'assets/12.jpeg',
+      title: 'Algorithms',
+      subtitle:
+          'Book created for educational purposes and is not affiliated with Algorithms group(s), company(s) nor Stack Overflow. All trademarks belong to their respective company owners',
+      totalRates: '⭐⭐⭐⭐',
+      totalFollowers: '400k',
+      totalDownloading: '12.2m',
+    )
+  ];
+}
---
+++ lib/models/profile.dart
@@ -0,0 +1,17 @@
+class Profile {
+  final String imageUrl;
+  final String title;
+  final String subtitle;
+  final String totalRates;
+  final String totalFollowers;
+  final String totalDownloading;
+
+  Profile({
+    this.imageUrl,
+    this.subtitle,
+    this.title,
+    this.totalFollowers,
+    this.totalDownloading,
+    this.totalRates,
+  });
+}
